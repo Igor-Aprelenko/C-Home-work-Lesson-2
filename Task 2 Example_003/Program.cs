ae36f78dbd9146d74d17e569c2f15a638ed49555
Igor Aprelenko <igor.aprelenko@mail.ru>
2023-06-15 17:29:23 +0300

    Задание 3 ко 2 семинару

---
+++ Task 2 Example_003/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу, которая принимает на вход цифру,обозначающую 
+//  день недели, и проверяет, является ли этот день выходным. 
+
+int ReadInt(string message)
+{
+System.Console.Write(message); 
+int number = Convert.ToInt32(Console.ReadLine()); 
+return number; 
+}
+int day = ReadInt("Введите номер дня недели -> ");
+
+if (day > 7)
+{
+    System.Console.WriteLine("Такого дня недели не существует");
+}
+else if (day ==6 || day == 7)
+{
+    System.Console.WriteLine("Ура выходной");
+}
+else 
+{
+    System.Console.WriteLine("Увы будний день");
+}
+
+
+
