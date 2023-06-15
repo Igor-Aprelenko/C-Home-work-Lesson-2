d96ecaec48911a6fb7666ce438f5d7cd3516107f
Igor Aprelenko <igor.aprelenko@mail.ru>
2023-06-15 17:28:47 +0300

    Задание 1 ко 2 семинару

---
+++ Task 2 Example_001/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+// Не использовать строки
+
+int ReadInt(string message)
+{
+System.Console.Write(message); 
+int number = Convert.ToInt32(Console.ReadLine()); 
+return number; 
+}
+int num = ReadInt("Введите 3-х значное число -> ");
+if (num < 100 || num >= 1000)
+{
+System.Console.WriteLine("Число не 3-х значное");
+}
+else
+{
+int digit = num / 10 % 10;
+System.Console.WriteLine($"Вторая цифра числа равна {digit}");
+}
+
+
